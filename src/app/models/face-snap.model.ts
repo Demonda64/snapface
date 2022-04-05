ecf773ca45eab8b3f3800ff314cecfe4dea08314
Oscar64 <oscardemonda@outlook.fr>
2022-04-05 16:13:59 +0200

    Ajoutez des propriétés personnalisées

---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,8 @@
+export class FaceSnap{
+    constructor( public title: string,
+                 public description: string, 
+                 public imgURL: string, 
+                 public createDate: Date, 
+                 public snaps: number){
+    }
+}
