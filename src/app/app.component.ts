e8d1083bc49064326d59125a937ec7f6a55a672f
Oscar64 <oscardemonda@outlook.fr>
2022-04-11 10:25:46 +0200

    Centraliser les interactions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
+
 
 @Component({
   selector: 'app-root',
@@ -8,37 +8,7 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-  faceSnaps!:FaceSnap[];
-
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title:' Freezer ',
-        description:' Je deteste les sayans',
-        imgURL:'https://i.pinimg.com/564x/85/96/37/8596379e5e203ec3987dd8eab9eda786.jpg',
-        createDate:new Date(),
-        snaps :150,
-        location: ' sur Namek  💥  '
-      },
-      {
-        title:' Goku ',
-        description:' Je veux manger et me battre',
-        imgURL:'https://i.pinimg.com/564x/62/e9/26/62e926a90388189011b0e81a13b06349.jpg',
-        createDate:new Date(),
-        snaps :0,
-        location:' chez Maitre Whis 🪐 '
-
-      },
-      {
-        title:' Vegeta ',
-        description:' Je suis le prince des sayans',
-        imgURL:'https://i.pinimg.com/564x/f2/5e/37/f25e37c475d2e5e3f87e1fda5b58f788.jpg',
-        createDate:new Date(),
-        snaps :0
-
-      },
-    ];    
-  }
+  ngOnInit() {}
 }
     
     
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from "@angular/core";
+import { FaceSnap } from "../models/face-snap.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FaceSnapsService {
+    faceSnaps: FaceSnap[] = [
+        {
+          id:1,
+          title:' Freezer ',
+          description:' Je deteste les sayans',
+          imgURL:'https://i.pinimg.com/564x/85/96/37/8596379e5e203ec3987dd8eab9eda786.jpg',
+          createDate:new Date(),
+          snaps :150,
+          location: ' sur Namek  💥  '
+        },
+        {
+          id: 2,
+          title:' Goku ',
+          description:' Je veux manger et me battre',
+          imgURL:'https://i.pinimg.com/564x/62/e9/26/62e926a90388189011b0e81a13b06349.jpg',
+          createDate:new Date(),
+          snaps :0,
+          location:' chez Maitre Whis 🪐 '
+  
+        },
+        {
+          id : 3,
+          title:' Vegeta ',
+          description:' Je suis le prince des sayans',
+          imgURL:'https://i.pinimg.com/564x/f2/5e/37/f25e37c475d2e5e3f87e1fda5b58f788.jpg',
+          createDate:new Date(),
+          snaps :0
+  
+        },
+        {
+            id:4,
+            title:' Freezer ',
+            description:' Je deteste les sayans',
+            imgURL:'https://i.pinimg.com/564x/85/96/37/8596379e5e203ec3987dd8eab9eda786.jpg',
+            createDate:new Date(),
+            snaps :150,
+            location: ' sur Namek  💥  '
+          },
+          {
+            id: 5,
+            title:' Goku ',
+            description:' Je veux manger et me battre',
+            imgURL:'https://i.pinimg.com/564x/62/e9/26/62e926a90388189011b0e81a13b06349.jpg',
+            createDate:new Date(),
+            snaps :0,
+            location:' chez Maitre Whis 🪐 '
+    
+          },
+          {
+            id : 6,
+            title:' Vegeta ',
+            description:' Je suis le prince des sayans',
+            imgURL:'https://i.pinimg.com/564x/f2/5e/37/f25e37c475d2e5e3f87e1fda5b58f788.jpg',
+            createDate:new Date(),
+            snaps :0
+    
+          },
+      ];
+
+      getAllFaceSnaps(): FaceSnap[] {
+          return this.faceSnaps;
+      }
+
+}
